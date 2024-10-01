2db77dd4e8ad825b4596b6832a15adb69534f21b
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-10-01 01:26:59 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// Jogo adivinha com POO
+alert('Jogo do advinha JS Game!!');
+
+// Links de referencia do projeto para sortear numeros aleatorio-> https://github.com/alura-cursos/logica-js
+
+class Game {
+	// atributos
+	nomeJogador;
+	numeroSorte;
+	//metodo contructor
+	constructor(nomeJogador) {
+		this.nomeJogador = nomeJogador;
+		// https://devfuria.com.br/javascript/numeros-aleatorios/
+		this.numeroSorte = Math.floor(Math.random() * 10 + 1);
+	}
+	// metodos da class
+	startGame() {
+		this.nomeJogador = this.getNomeJogador()
+		let numeroSorteMaquina = this.soteiaNumero
+		console.log(numeroSorteMaquina)
+		let numeroTentativa = 1;
+		let numeroEscolhaJogador = this.entradaUsuarioNumeroEscolhido();
+		while (numeroEscolhaJogador != numeroSorteMaquina) {
+
+			if (numeroEscolhaJogador >= numeroSorteMaquina) {
+
+				alert('o numero é menor!');
+				numeroEscolhaJogador = this.entradaUsuarioNumeroEscolhido();
+			} else {
+
+				alert('o numero é maior!');
+				numeroEscolhaJogador = this.entradaUsuarioNumeroEscolhido();
+			}
+			numeroTentativa++;
+		}
+		let tentativa = numeroTentativa == 1 ? 'tentativa' : 'tentativas';
+		alert(`${this.nomeJogador}, Você acertou em ${numeroTentativa} ${tentativa}, o numero da sorte é  ${numeroSorteMaquina}!`);
+	}
+	// numero escolhido
+	entradaUsuarioNumeroEscolhido() {
+		let numeroEscolhido = parseInt(prompt('Escolhar um numero entre 1 e 10.'));
+		if (numeroEscolhido == "") {
+			alert("Numero não pode ser vazio")
+			numeroEscolhido = parseInt(prompt('Escolhar um numero entre 1 e 10.'));
+		}
+		return numeroEscolhido;
+	}
+	// nome do usuario
+	getNomeJogador() {
+		let nome = prompt('Qual seu nome?.');
+		if (nome == "") {
+			alert("nome não pode ser vazio");
+			nome = prompt('Qual seu nome?.');
+		}
+		return nome;
+	}
+	// sorteia numero
+	get soteiaNumero() {
+		return this.numeroSorte;
+	}
+}
+
+const player1 = new Game("player1");
+player1.startGame();
